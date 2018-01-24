2c38bb4a1dc92010c2fef325ab3a9599c0b5b671
Garrett Berg <googberg@gmail.com>
2018-01-24 09:52:46 -0700

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -114,9 +114,10 @@ use std::convert::AsRef;
 use std::io;
 use std::fmt;
 use std::ops::Deref;
-use std::path::{Path, PathBuf};
+use std::path::{Path, PathBuf, StripPrefixError};
 use std::sync::Arc;
 
+mod abs;
 mod dir;
 mod file;
 mod open;
@@ -124,19 +125,23 @@ mod open;
 mod ser;
 mod ty;
 
+pub use abs::PathAbs;
 pub use dir::PathDir;
 pub use file::PathFile;
 pub use open::PathOpen;
 pub use ty::PathType;
 
 #[derive(Clone, Eq, Hash, PartialEq, PartialOrd, Ord)]
-/// An absolute ([canonicalized][1]) path that is guaranteed (when created) to exist.
+/// A `PathBuf` that is atomically reference counted and reimplements the `PathBuf`
+/// methods to display the action and path when there is an error.
 ///
-/// [1]: https://doc.rust-lang.org/std/path/struct.Path.html?search=#method.canonicalize
-pub struct PathAbs(Arc<PathBuf>);
+/// This is the root type of all other `Path*` types in this crate.
+///
+/// This type is also serializable when the `serialize` feature is enabled.
+pub struct PathArc(Arc<PathBuf>);
 
-impl PathAbs {
-    /// Instantiate a new `PathAbs`. The path must exist or `io::Error` will be returned.
+impl PathArc {
+    /// Instantiate a new `PathArc`.
     ///
     /// # Examples
     /// ```rust
@@ -144,104 +149,56 @@ impl PathAbs {
     /// use path_abs::PathAbs;
     ///
     /// # fn main() {
-    /// let lib = PathAbs::new("src/lib.rs").unwrap();
+    /// let path = PathArc::new("some/path");
+    /// let path2 = path.clone(); // cloning is cheap
     /// # }
     /// ```
-    pub fn new<P: AsRef<Path>>(path: P) -> io::Result<PathAbs> {
-        let can = path.as_ref().canonicalize().map_err(|err| {
-            io::Error::new(
-                err.kind(),
-                format!("{} when cannonicalizing {}", err, path.as_ref().display()),
-            )
-        })?;
-
-        Ok(PathAbs(Arc::new(can)))
+    pub fn new<P: AsRef<Path>>(path: P) -> PathArc {
+        PathArc::from_buf(path.as_ref().to_path_buf())
     }
 
-    /// Resolve the `PathAbs` as a `PathFile`. Return an error if it is not a file.
-    pub fn into_file(self) -> io::Result<PathFile> {
-        PathFile::from_abs(self)
+    /// Instantiate a new `PathArc` from a `PathBuf`.
+    pub fn from_buf(path: PathBuf) -> PathArc {
+        PathArc(Arc::new(path))
     }
 
-    /// Resolve the `PathAbs` as a `PathDir`. Return an error if it is not a directory.
-    pub fn into_dir(self) -> io::Result<PathDir> {
-        PathDir::from_abs(self)
-    }
-
-    /// Get the parent directory of this path as a `PathDir`.
-    ///
-    /// > This does not make additinal syscalls, as the parent by definition must be a directory
-    /// > and exist.
-    ///
-    /// # Examples
-    /// ```rust
-    /// # extern crate path_abs;
-    /// use path_abs::{PathDir, PathFile};
-    ///
-    /// # fn main() {
-    /// let lib = PathFile::new("src/lib.rs").unwrap();
-    /// let src = lib.parent_dir().unwrap();
-    /// assert_eq!(PathDir::new("src").unwrap(), src);
-    /// # }
-    /// ```
-    pub fn parent_dir(&self) -> Option<PathDir> {
-        match self.parent() {
-            Some(p) => Some(PathDir(PathAbs(Arc::new(p.to_path_buf())))),
-            None => None,
-        }
-    }
-
-    /// For constructing mocked paths during tests. This is effectively the same as a `PathBuf`.
-    ///
-    /// This is NOT checked for validity so the file may or may not actually exist and will
-    /// NOT be, in any way, an absolute or canonicalized path.
-    ///
-    /// # Examples
-    /// ```rust
-    /// # extern crate path_abs;
-    /// use path_abs::PathAbs;
-    ///
-    /// # fn main() {
-    /// // this file exist
-    /// let lib = PathAbs::new("src/lib.rs").unwrap();
-    ///
-    /// let lib_mocked = PathAbs::mock("src/lib.rs");
-    ///
-    /// // in this case, the mocked file exists
-    /// assert!(lib_mocked.exists());
-    ///
-    /// // However, it is NOT equivalent to `lib`
-    /// assert_ne!(lib, lib_mocked);
-    ///
-    /// // this file doesn't exist at all
-    /// let dne = PathAbs::mock("src/dne.rs");
-    /// assert!(!dne.exists());
-    /// # }
-    /// ```
-    pub fn mock<P: AsRef<Path>>(fake_path: P) -> PathAbs {
-        PathAbs(Arc::new(fake_path.as_ref().to_path_buf()))
-    }
+    // /// Returns a path that, when joined onto base, yields self.
+    // ///
+    // /// This function is identical to [std::path::Path::strip_prefix][0] except
+    // /// it has error messages which include the action and the path
+    // ///
+    // /// [0]: https://doc.rust-lang.org/std/path/struct.Path.html#method.strip_prefix
+    // pub fn strip_prefix<'a, P>(&'a self, base: &'a P) -> Result<&'a Path, StripPrefixError>
+    //     where P: AsRef<Path>
+    // {
+    //     self.0.strip_prefix(base).map_err(|err| {
+    //         io::Error::new(
+    //             err.kind(),
+    //             format!("{} when stripping prefix of {}", err, self.display()),
+    //         )
+    //     })
+    // }
 }
 
-impl fmt::Debug for PathAbs {
+impl fmt::Debug for PathArc {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         self.0.fmt(f)
     }
 }
 
-impl AsRef<PathBuf> for PathAbs {
+impl AsRef<PathBuf> for PathArc {
     fn as_ref(&self) -> &PathBuf {
         &self.0
     }
 }
 
-impl AsRef<Path> for PathAbs {
+impl AsRef<Path> for PathArc {
     fn as_ref(&self) -> &Path {
         self.0.as_path()
     }
 }
 
-impl Deref for PathAbs {
+impl Deref for PathArc {
     type Target = PathBuf;
 
     fn deref(&self) -> &PathBuf {
